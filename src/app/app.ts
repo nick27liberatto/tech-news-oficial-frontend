6c42de107fc7dd7e723a5cbdd4dfab30902b7606
Nicolas Liberatto <nicolas.liberatto@gmail.com>
2025-08-18 18:48:31 -0300

    Pause Option Added to Background Video

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('auth-system');
+  @ViewChild('bgVideo') bgVideo!: ElementRef<HTMLVideoElement>;
+  isPaused:boolean = false;
+  iconPath:string = '/assets/images/pause-icon.png';
+
+  pauseVideo(){
+    const video = this.bgVideo.nativeElement;
+
+    if(video.paused) {
+      video.play();
+      this.isPaused = false;
+      this.iconPath = '/assets/images/pause-icon.png';
+    } else {
+      video.pause();
+      this.isPaused = true;
+      this.iconPath = '/assets/images/play-icon.png';
+    }
+  }
 }
