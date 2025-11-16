2ef73a9bf7bd06ce214778ba3418ebbb9b2e44d5
Nicolas Liberatto <nicolas.liberatto@gmail.com>
2025-11-16 17:04:50 -0300

    TNO-10 Environment Variables Fixed

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -7,16 +7,21 @@ import { environment } from '../../environments/environments.prod';
 })
 export class SupabaseService {
   private supabaseclient: SupabaseClient;
+  private SUPABASE_URL: string = process.env['SUPABASE_URL'] ?? environment.supabase.url;
+  private SUPABASE_KEY: string = process.env['SUPABASE_KEY'] ?? environment.supabase.key;
+  
   constructor() {
-    this.supabaseclient = createClient(environment.SUPABASE_URL, environment.SUPABASE_KEY);
+    this.supabaseclient = createClient(this.SUPABASE_URL, this.SUPABASE_KEY);
   }
 
   signUp(fullName: string, email: string, password: string) {
-    return this.supabaseclient.auth.signUp({ email, password, options: {
-      data: {
-        display_name: fullName
+    return this.supabaseclient.auth.signUp({
+      email, password, options: {
+        data: {
+          display_name: fullName
+        }
       }
-    } });
+    });
   }
 
   signIn(email: string, password: string) {
